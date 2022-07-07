e3dfac8908d9c2375c691342223a4474c2ef6d7f
Sandhya-24 <108857379+Sandhya-24@users.noreply.github.com>
2022-07-07 11:58:22 +0530

    Update Compute_the_Probability_of_an_Outcome_Given_a_Hidden_Path.py

--- Compute_the_Probability_of_an_Outcome_Given_a_Hidden_Path.py
+++ Compute_the_Probability_of_an_Outcome_Given_a_Hidden_Path.py
@@ -1,22 +1,20 @@
-lines = []
 infile='/Users/sandhyagovindarajan/PycharmProjects/Rosalind /HMM'
 with open(infile) as fp:
     lines=fp.read().split()
-    hidden_states=lines[0]
-    observed_states=lines[6]
-    hidden_states_alpha=[]
-    hidden_states_alpha=lines[2:5]
-    observed_states_alpha=lines[8:10]
-    hidden=list(map(float,lines[15:18]))
-    obs=list(map(float,lines[19:]))
+    print(lines)
+    observed_states=lines[0]
+    hidden_states=lines[6]
+    hidden_states_alpha=lines[8:10]
+    observed_states_alpha=lines[2:5]
+    line1=list(map(float,lines[15:18]))
+    line2=list(map(float,lines[19:]))
     emission_matrix=[]
-    emission_matrix.append(hidden)
-    emission_matrix.append(obs)
-    print(emission_matrix)
+    emission_matrix.append(line1)
+    emission_matrix.append(line2)
 
 p=1
 for i in range(len(observed_states)):
     obs_ind=observed_states_alpha.index(observed_states[i])
     hid_ind=hidden_states_alpha.index(hidden_states[i])
-    p*=emission_matrix[obs_ind][hid_ind]
+    p*=emission_matrix[hid_ind][obs_ind]
 print(p)
