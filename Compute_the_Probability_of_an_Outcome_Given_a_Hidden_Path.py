04d7abb8e05c174b89dd68ece8beccdf72830fe9
Sandhya-24 <108857379+Sandhya-24@users.noreply.github.com>
2022-07-07 12:20:47 +0530

    Update Compute_the_Probability_of_an_Outcome_Given_a_Hidden_Path.py
    Another solution

--- Compute_the_Probability_of_an_Outcome_Given_a_Hidden_Path.py
+++ Compute_the_Probability_of_an_Outcome_Given_a_Hidden_Path.py
@@ -1,20 +1,13 @@
+from io import StringIO
+import pandas as pd
 infile='/Users/sandhyagovindarajan/PycharmProjects/Rosalind /HMM'
 with open(infile) as fp:
-    lines=fp.read().split()
-    print(lines)
-    observed_states=lines[0]
-    hidden_states=lines[6]
-    hidden_states_alpha=lines[8:10]
-    observed_states_alpha=lines[2:5]
-    line1=list(map(float,lines[15:18]))
-    line2=list(map(float,lines[19:]))
-    emission_matrix=[]
-    emission_matrix.append(line1)
-    emission_matrix.append(line2)
-
+    lines = fp.read().split('\n--------\n')
+    obs_state=lines[0]
+    hidden_states=lines[2]
+    table=StringIO(lines[4])
+    fin_table=pd.read_table(table,sep='\s+')
 p=1
-for i in range(len(observed_states)):
-    obs_ind=observed_states_alpha.index(observed_states[i])
-    hid_ind=hidden_states_alpha.index(hidden_states[i])
-    p*=emission_matrix[hid_ind][obs_ind]
+for h,obs in zip(hidden_states,obs_state):
+    p*=fin_table.loc[h,obs]
 print(p)
